99525746358b2c9cc910d0af00dafc98e11316e2
ThlonDev <yuthlon157@gmail.com>
2026-02-25 22:13:52 +0900

    Separator Handler

---
+++ handler.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func handler() {
+	http.HandleFunc("GET /about/", handleAbout)
+	http.HandleFunc("GET /users/", handleUsers)
+}
+
+func handleAbout(w http.ResponseWriter, r *http.Request) {
+	aboutHTML, err := os.ReadFile("./html/index.html")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Internal Server Error 500"))
+		return
+	}
+	w.Write(aboutHTML)
+
+	fmt.Printf("Received request from %s\n", r.RemoteAddr)
+}
+
+func handleUsers(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Users"))
+	fmt.Printf("Received request from %s\n", r.RemoteAddr)
+}
--- main.go
+++ main.go
@@ -1,41 +1,14 @@
 package main
 
 import (
-	"flag"
-	"fmt"
 	"net/http"
-	"os"
 )
 
 func main() {
-	processArgs()
 	server := http.Server{
 		Addr: ":8080", Handler: nil}
 
-	http.HandleFunc("GET /about/", handleAbout)
-	http.HandleFunc("GET /users/", handleUsers)
+	handler()
 
 	server.ListenAndServe()
-}
-
-func processArgs() {
-	port := 8080
-	flag.IntVar(&port, "port", 8080, "Port to listen on")
-}
-
-func handleAbout(w http.ResponseWriter, r *http.Request) {
-	aboutHTML, err := os.ReadFile("./html/index.html")
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("Internal Server Error 500"))
-		return
-	}
-	w.Write(aboutHTML)
-
-	fmt.Printf("Received request from %s\n", r.RemoteAddr)
-}
-
-func handleUsers(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Users Page"))
-	fmt.Printf("Received request from %s\n", r.RemoteAddr)
 }
